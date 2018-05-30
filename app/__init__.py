f036122a1b252eb10a4afe3d7b489ffccfd08d3a
asamisellah <asami.rachael@gmail.com>
2018-05-30 22:15:24 +0300

    [Feature #157965217] Create API Endpoints The following are the endpoints created: - Get users/requests - Get users/requests/requestId - Put users/requests/requestId - Post users/requests

--- app/__init__.py
+++ app/__init__.py
@@ -1,58 +1,101 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-user = [
-    {
-        "name": "betty",
-        "email": "betty@email.com",
-        "password": "qwe123",
-        "confirm_password": "qwe123",
-        "request": [
-            {
-                "title": "Leaking pipe",
-                "type": "Repair",
-                "description": "Rusty pipe",
-                "category": "plumbing",
-                "area": "Block A"
-            }
-        ]
+
+requests = [{}]
+
+users = [{}]
+
+
+# POST a user that belongs to logged in user
+@app.route('/users', methods=['POST'])
+def create_user():
+    user = {
+        "username": request.json.get("username"),
+        "email": request.json.get('email'),
+        "password": request.json.get("password"),
+        "cconfirm_password": request.json.get('cconfirm_password'),
     }
-]
+    users.append(user)
+    return jsonify({"users": users}), 201
 
 
-# Create user
-@app.route('/user', method=['POST'])
-def create(user):
-    pass
+# GET a user
+@app.route('/users/<string:userId>')
+def get_user(userId):
+    user = [user for user in users if user["id"] == userId]
+    if len(user) == 0:
+        return jsonify({"message": "Not Found"}), 404
+
+    else:
+        return jsonify({"user": user})
 
 
-# # Get user
-# @app.route('/user/<string:name>')
-# def get_user(user):
-#     return jsonify({"user": user})
+# GET all requests that belong to a logged in user
+@app.route('/requests')
+def get_requests():
+    return jsonify({"request": requests})
 
 
-# Get all users
-@app.route('/user')
-def get_users(user):
-    return jsonify({"user": user})
+# GET a request that belong to a logged in user
+@app.route('/users/requests/<string:requestId>')
+def get_request(requestId):
+    request = [request for request in requests if request["id"] == requestId]
+    if len(request) == 0:
+        return jsonify({"message": "Not Found"}), 404
+
+    else:
+        return jsonify({"request": request})
 
 
-# # Create user request
-# @app.route('/user/<string:name>/request', method=['POST'])
-# def create_request():
-#     pass
+# GET a request that belongs to a logged in user
+@app.route('/users/requests/<string:requestId>')
+def get_request(requestId):
+    request = [request for request in requests if request["id"] == requestId]
+    if len(request) == 0:
+        return jsonify({"message": "Not Found"}), 404
+
+    else:
+        return jsonify({"request": request})
 
 
-# # Get user request
-# @app.route('/user/<string:name>/request/<string:title>')
-# def get_request():
-#     pass
+# POST a request that belongs to logged in user
+@app.route('/users/requests', methods=['POST'])
+def create_request():
+    request_data = {
+        "title": request.json.get("title"),
+        "type": request.json.get('type'),
+        "description": request.json.get("description"),
+        "category": request.json.get('category'),
+    }
+    requests.append(request)
+    return jsonify({"requests": requests}), 201
 
 
-# # Get all user requests
-# @app.route('/user/<string:name>/request')
-# def get_requests():
-#     pass
+# UPDATE(PUT) a request that belongs to logged in user
+@app.route('/users/requests/<string:requestId>', methods=['PUT'])
+def modify_request(requestId):
+    request = [request for request in requests if requests["id"] == requestId]
+    if len(request) == 0:
+        return jsonify({"message": "Not Found"})
+    else:
+        request = {
+            "title": request.json.get("title"),
+            "type": request.json.get('type'),
+            "description": request.json.get("description"),
+            "category": request.json.get('category'),
+            "area": request.json.get('area')
+        }
+        
+        requests.append(request)
+        return jsonify({"request": request}), 201
 
+
+# DELETE a request made by a logged in user
+@app.route('/users/requests/<string:requestId>', methods=['DELETE'])
+def delete_request(requestId):
+    request = [request for request in requests if request["id"] == requestId]
+    print(request[0])
+    requests.remove(request[0])
+    return jsonify({"request": requests}), 204
