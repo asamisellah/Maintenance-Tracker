a3ec2315edd688591189dfe47e870f6206aacf17
asamisellah <asami.rachael@gmail.com>
2018-05-30 10:25:31 +0300

    [Bug #157930395]Fix test errors

--- test_requests.py
+++ test_requests.py
@@ -3,38 +3,38 @@ import unittest
 
 
 class TestRequests(unittest.TestCase):
-
-    def setUp(self):
-        app.config["TESTING"] = True
-        self.client = app.test_client()
-
-    def create_request(self, title, type, description, category, area):
-        request = {"title": "Leaking Pipe",
+    request = {"title": "Leaking Pipe",
                    "type": "Repair",
                    "description": "Some description",
                    "category": "Plumbing",
                    "area": "Block A"}
 
-        return self.client.post('/users/1/request', data=request)
+    def setUp(self):
+        app.config["TESTING"] = True
+        self.client = app.test_client()
+        
+    def create_request(self, title, type, description, category, area):
+
+        return self.client.post('/users/requests', data=self.request)
 
     def test_create_request(self):
-        res = client.post('/users/requests', data=request)
+        res = self.client.post('/users/requests', data=self.request)
         self.assertEqual(res.status_code, 201)
 
     def test_update_request(self):
-        res = client.put('/users/requests/1', data=request)
+        res = self.client.put('/users/requests/1', data=self.request)
         self.assertEqual(res.status_code, 200)
 
     def test_all_requests(self):
-        res = client.get('/users/requests')
+        res = self.client.get('/users/requests')
         self.assertEqual(res.status_code, 200)
 
     def test_get_request(self):
-        res = client.get('/users/requests/1')
+        res = self.client.get('/users/requests/1')
         self.assertEqual(res.status_code, 200)
 
     def test_delete_request(self):
-        res = client.delete('/users/request/1')
+        res = self.client.delete('/users/request/1')
         self.assertEqual(res.status_code, 200)
 
 
--- test_users.py
+++ test_users.py
@@ -1,61 +1,55 @@
 from app import app
 import unittest
 
+
 class TestUsers(unittest.TestCase):
+    users = {"name": "Betty", "email": "betty@email.com",
+             "password": "qwe123", "confirm_password": "qwe123"}
 
     def setUp(self):
         self.client = app.test_client()
 
-    def tearDown(self):
-        self.users.clear()
-
     def register_user(self, name, email, password, confirm_password):
-        users = {name: "Betty",
-                email: "betty@email.com", 
-                password: "qwe123",
-                confirm_password: "qwe123"}
         
-        return self.client.post('/register', data = users)
+        return self.client.post('/register', data=self.users)
 
     def test_registration(self):
-        res = client.post('/register', data = users)
+        res = self.client.post('/register', data=self.users)
         self.assertEqual(res.status_code, 201)
 
-    
     def signin_user(self, email, password):
         user_account = {email: "betty@email.com", password: "qwe123"}
 
-        return self.client.post('/sign_in', data = user_account)
+        return self.client.post('/sign_in', data=self.users)
 
     def test_sign_in(self):
-        res = client.post('/sign_in', data = user_account)
+        res = self.client.post('/sign_in', data=self.users)
         self.assertEqual(res.status_code, 200)
 
-    
     def test_update_user(self):
-        res = client.put('/users/1', data = request)
+        res = self.client.put('/users/1', data=self.users)
         self.assertEqual(res.status_code, 200)
 
     def test_all_users(self):
-        res = client.get('/users')
+        res = self.client.get('/users')
         self.assertEqual(res.status_code, 200)
     
     def test_get_user(self):
-        res = client.get('/users/1')
+        res = self.client.get('/users/1')
         self.assertEqual(res.status_code, 200)
 
     def test_delete_user(self):
-        res = client.delete('/users/1')
+        res = self.client.delete('/users/1')
         self.assertEqual(res.status_code, 200)
 
     # Test for edge-cases    
 
     def test_blank_registration(self):
-        res = client.post('/register', data = register_user("", "", "", ""))
+        res = self.client.post('/register', data="")
         self.assertEqual(res.status_code, 400)
 
     def test_signin_unregistered_user(self):
-        res = client.post('/sign_in', data = signin_user("Betty", "qwe123"))
+        res = self.client.post('/sign_in', data=self.users)
         self.assertEqual(res.status_code, 401)
 
         
