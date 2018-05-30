187c1b805af179f4949d91ebf04fafaca9b87761
asamisellah <asami.rachael@gmail.com>
2018-05-30 15:36:32 +0300

    [Feature #157965217]Structure endpoint routes and methods

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,47 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+user = [
+    {
+        "name": "betty",
+        "email": "betty@email.com",
+        "password": "qwe123",
+        "confirm_password": "qwe123"
+        "request": [
+            {
+                "title": "Leaking pipe"
+                "type": "Repair"
+                "description": "Rusty pipe"
+                "category": "plumbing"
+                "area": "Block A"
+            }
+        ]
+    }
+]
+
+
+@app.route('/user', method=['POST'])
+def create(user):
+    get
+
+
+    # @app.route('user/<string:name>')
+    # def get_user(name):
+    #     pass
+
+    # @app.route('/user')
+    # def get_users():
+    #     pass
+
+    # @app.route('/user/<string:name>/request', method=['POST'])
+    # def create_request():
+    #     pass
+
+    # @app.route('/user/<string:name>/request')
+    # def get_requests():
+    #     pass
+
+    # @app.route('/user/<string:name>/request/<string:id>')
+    # def get_request():
+    #     pass
