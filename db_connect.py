05f8f6f5c54f8364480280f591d0a187b008d712
asamisellah <asami.rachael@gmail.com>
2018-06-11 10:11:01 +0300

    [Fixes]]

--- db_connect.py
+++ db_connect.py
@@ -13,7 +13,7 @@ class TrackerDB(object):
             self.init_app(app)
 
     def init_app(self, app):
-        self.conn = connect(app.config['DATABASE_URL'])
+        self.conn = connect(postgres: // cprwgabrqpkdmg: 1821cee79a9993b605b275bd608c135fa4e17ded697b53515bb1f7ddf4baa0c0@ec2-50-19-224-165.compute-1.amazonaws.com: 5432/d7bp9i00oopfpn)
         self.cur = self.conn.cursor(cursor_factory=RealDictCursor)
 
     def query(self, query):
